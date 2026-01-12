901ad8498715241808b9a54d589a05d8a9b7b6c8
rbhargav0104 <by7785@srmist.edu.in>
2026-01-12 21:47:35 +0530

    Completed Two-Pointers-2

---
+++ search-2d-matrix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int i = 0;
+        int j = n - 1;
+        while(i < m && j >=0){
+            if(matrix[i][j] == target)return true;
+            else if(matrix[i][j] > target){
+                j--;//move towards the smaller elements 
+            }else{
+                i++;//move towards the larger elements
+            }
+        }
+
+        return false;//element not found
+    }
+};
